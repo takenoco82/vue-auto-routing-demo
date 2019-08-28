5c9bdf4be673033b053a2c396057886caf675a70
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-28 21:11:05 +0900

    [fix] 複数レイアウトを使えるようにした

--- demo-project/src/pages/login.vue
+++ demo-project/src/pages/login.vue
@@ -3,3 +3,10 @@
     <h1>This is the login page</h1>
   </div>
 </template>
+
+<script>
+export default {
+    // You can specify layout component name here (default value is 'default')
+    layout: "free"
+}
+</script>
